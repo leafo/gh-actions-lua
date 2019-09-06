6a81338eddbc73666827d7dc8c9a6b6132699941
leaf corcoran <leafot@gmail.com>
2019-09-05 22:53:03 -0700

    try some more

--- install-lua/main.js
+++ install-lua/main.js
@@ -1,9 +1,32 @@
 
-const core = require('@actions/core')
-const luaVersion = core.getInput('luaVersion')
+const core = require("@actions/core")
+const exec = require("@actions/exec")
+const io = require('@actions/io')
+const tc = require('@actions/tool-cache')
 
-console.log("hello world? " + luaVersion)
-core.warning("this is a warning");
+
+const INSTALL_PREFIX = ".install"
+
+const luaVersion = core.getInput('luaVersion', {required: true})
+
+await exec.exec("sudo apt-get install libreadline-dev")
+await io.mkdirP(INSTALL_PREFIX)
+
+const luaSourceTar = await tc.downloadTool(`http://www.lua.org/ftp/lua-${luaVersion}.tar.gz`)
+console.log(`source tar: ${luaSourceTar}`)
+const luaPath = await tc.extractTar(luaSourceTar, `${INSTALL_PREFIX}/lua-${luaVersion}`)
+console.log(`extract path: ${luaPath}`)
+
+// mkdir -p .install
+// LUA_INSTALL_DIR="$(pwd)/.lua"
+// mkdir -p "$LUA_INSTALL_DIR"
+// cd .install
+// curl http://www.lua.org/ftp/lua-${{ matrix.lua_version }}.tar.gz | tar xz
+// cd lua-${{ matrix.lua_version }}
+// make -j linux
+// make -j INSTALL_TOP="$LUA_INSTALL_DIR" install;
+
+core.warning("this is a warning..HELLO");
 
 
 
