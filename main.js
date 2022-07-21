0b9a58f5fb29964cbb8d141756eef1168f0cb7e9
Hisham Muhammad <hisham@gobolinux.org>
2022-07-21 18:29:16 -0300

    Compile plain Lua on Windows using MSVC
    Build manually since the provided Makefile is Unix-only.
    
    Based on similar Python code implemented by hererocks:
    https://github.com/luarocks/hererocks/blob/e44d384288c5385cdcfda92e662756fcd2d9eb8a/hererocks.py

--- main.js
+++ main.js
@@ -23,23 +23,28 @@ const VERSION_ALIASES = {
 }
 
 const isMacOS = () => (process.platform || "").startsWith("darwin")
+const isWindows = () => (process.platform || "").startsWith("win32")
+
+const exists = (filename, mode) => fsp.access(filename, mode).then(() => true, () => false)
+
+// Returns posix path for path.join()
+const pathJoin = path.posix.join
 
 // Returns posix path for process.cwd()
 const processCwd = () => {
-  const p = require("path");
-  return process.cwd().split(p.sep).join(p.posix.sep);
+  return process.cwd().split(path.sep).join(path.posix.sep);
 }
 
 async function finish_luajit_install(src, dst, luajit) {
   if (isWindows()) {
-    await fsp.copyFile(path.join(src, "lua51.dll"), path.join(dst, "bin", "lua51.dll"))
+    await fsp.copyFile(pathJoin(src, "lua51.dll"), pathJoin(dst, "bin", "lua51.dll"))
 
     await exec.exec(`ln -s ${luajit} lua.exe`, undefined, {
-      cwd: path.join(dst, "bin")
+      cwd: pathJoin(dst, "bin")
     })
   } else {
     await exec.exec(`ln -s ${luajit} lua`, undefined, {
-      cwd: path.join(dst, "bin")
+      cwd: pathJoin(dst, "bin")
     })
   }
 }
@@ -65,18 +70,18 @@ async function install_luajit_openresty(luaInstallPath) {
   }
 
   await exec.exec(`make ${finalCompileFlags}`, undefined, {
-    cwd: path.join(buildPath, "luajit2")
+    cwd: pathJoin(buildPath, "luajit2")
   })
 
   await exec.exec(`make -j install PREFIX="${luaInstallPath}"`, undefined, {
-    cwd: path.join(buildPath, "luajit2")
+    cwd: pathJoin(buildPath, "luajit2")
   })
 
-  await finish_luajit_install(path.join(installPath, "luajit2", "src"), luaInstallPath, "luajit-2.1.0-beta3")
+  await finish_luajit_install(pathJoin(buildPath, "luajit2", "src"), luaInstallPath, "luajit-2.1.0-beta3")
 }
 
 async function install_luajit(luaInstallPath, luajitVersion) {
-  const luaExtractPath = path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX, `LuaJIT-${luajitVersion}`)
+  const luaExtractPath = pathJoin(process.env["RUNNER_TEMP"], BUILD_PREFIX, `LuaJIT-${luajitVersion}`)
 
   const luaCompileFlags = core.getInput('luaCompileFlags')
 
@@ -102,16 +107,101 @@ async function install_luajit(luaInstallPath, luajitVersion) {
     cwd: luaExtractPath
   })
 
-  await finish_luajit_install(path.join(luaExtractPath, "src"), luaInstallPath, `luajit-${luajitVersion}`)
+  await finish_luajit_install(pathJoin(luaExtractPath, "src"), luaInstallPath, `luajit-${luajitVersion}`)
+}
+
+async function msvc_link(luaExtractPath, linkCmd, outFile, objs) {
+  await exec.exec(linkCmd + " /out:" + outFile, objs, {
+    cwd: luaExtractPath
+  })
+
+  let manifest = outFile + ".manifest"
+  if (await exists(manifest)) {
+    await exec.exec("mt /nologo", ["-manifest", manifest, "-outputresource:" + outFile], {
+      cwd: luaExtractPath
+    })
+  }
+}
+
+async function install_files(dstDir, srcDir, files) {
+  await io.mkdirP(dstDir)
+  for (let file of files) {
+    await fsp.copyFile(pathJoin(srcDir, file), pathJoin(dstDir, path.posix.basename(file)))
+  }
+}
+
+async function install_plain_lua_windows(luaExtractPath, luaInstallPath, luaVersion) {
+  const luaCompileFlags = core.getInput('luaCompileFlags')
+
+  let cl = "cl /nologo /MD /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE"
+
+  let objs = {
+    "lib": [],
+    "lua": [],
+    "luac": [],
+  }
+
+  let sources = {
+    "lua": [ "lua.c" ],
+    "luac": [ "luac.c", "print.c" ],
+  }
+
+  let src = pathJoin(luaExtractPath, "src")
+
+  await fsp.readdir(src).then(async (files) => {
+    for (let file of files) {
+      if (file.endsWith(".c")) {
+        let mode = sources["lua"].includes(file)
+                 ? "lua"
+                 : sources["luac"].includes(file)
+                 ? "luac"
+                 : "lib"
+
+        let srcName = pathJoin("src", file)
+
+        let args = (mode === "lib")
+                 ? [ "-DLUA_BUILD_AS_DLL", srcName ]
+                 : [ srcName ]
+
+        objs[mode].push(file.replace(".c", ".obj"))
+
+        await exec.exec(cl, args, {
+          cwd: luaExtractPath
+        })
+      }
+    }
+  })
+
+  objs["lua"] = [ ...objs["lua"], ...objs["lib"] ]
+  objs["luac"] = [ ...objs["luac"], ...objs["lib"] ]
+
+  let luaXYZ = luaVersion.split(".")
+  let libFile = "lua" + luaXYZ[0] + luaXYZ[1] + ".lib"
+  let dllFile = "lua" + luaXYZ[0] + luaXYZ[1] + ".dll"
+
+  await msvc_link(luaExtractPath, "link /nologo /DLL", dllFile, objs["lib"]);
+  await msvc_link(luaExtractPath, "link /nologo", "luac.exe", objs["luac"]);
+  await msvc_link(luaExtractPath, "link /nologo", "lua.exe", objs["lua"]);
+
+  const luaHpp = (await exists(pathJoin(src, "lua.hpp"))) ? "lua.hpp" : "../etc/lua.hpp"
+  const headers = [ "lua.h", "luaconf.h", "lualib.h", "lauxlib.h", luaHpp ]
+
+  await install_files(pathJoin(luaInstallPath, "bin"), luaExtractPath, [ "lua.exe", "luac.exe" ])
+  await install_files(pathJoin(luaInstallPath, "lib"), luaExtractPath, [ dllFile, libFile ])
+  await install_files(pathJoin(luaInstallPath, "include"), src, headers)
 }
 
 async function install_plain_lua(luaInstallPath, luaVersion) {
-  const luaExtractPath = path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX, `lua-${luaVersion}`)
+  const luaExtractPath = pathJoin(process.env["RUNNER_TEMP"], BUILD_PREFIX, `lua-${luaVersion}`)
   const luaCompileFlags = core.getInput('luaCompileFlags')
 
   const luaSourceTar = await tc.downloadTool(`https://www.lua.org/ftp/lua-${luaVersion}.tar.gz`)
   await io.mkdirP(luaExtractPath)
   await tc.extractTar(luaSourceTar, path.join(process.env["RUNNER_TEMP"], BUILD_PREFIX))
+
+  if (isWindows()) {
+    return await install_plain_lua_windows(luaExtractPath, luaInstallPath, luaVersion);
+  }
 
   if (isMacOS()) {
     await exec.exec("brew install readline ncurses")
@@ -153,7 +243,6 @@ async function install(luaInstallPath, luaVersion) {
 }
 
 const makeCacheKey = (luaVersion, compileFlags) => `lua:${luaVersion}:${process.platform}:${process.arch}:${compileFlags}`
-const exists = (filename, mode) => fsp.access(filename, mode).then(() => true, () => false)
 
 async function main() {
   let luaVersion = core.getInput('luaVersion', { required: true })
@@ -162,7 +251,7 @@ async function main() {
     luaVersion = VERSION_ALIASES[luaVersion]
   }
 
-  const luaInstallPath = path.join(processCwd(), LUA_PREFIX)
+  const luaInstallPath = pathJoin(processCwd(), LUA_PREFIX)
 
   let toolCacheDir = tc.find('lua', luaVersion)
 
@@ -195,7 +284,7 @@ async function main() {
     await fsp.symlink(toolCacheDir, luaInstallPath);
   }
 
-  core.addPath(path.join(luaInstallPath, "bin"))
+  core.addPath(pathJoin(luaInstallPath, "bin"))
 }
 
 main().catch(err => {
